bf9552d31f78aa4c6920677247b8d306b7c9ed51
Anterpreet Kaur <156046690+AnterpreetK@users.noreply.github.com>
2025-06-25 11:25:02 +0530

    Time: 1 ms (87.18%), Space: 41.2 MB (31.35%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,15 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int) Math.sqrt(x);
+        if(x == 1) return x;
+
+        int l = 1, r = x/2;
+        while(l <= r){
+            int m = (l+r) / 2;
+            long sq = (long)m * m;
+            if(sq == x) return m;
+            else if(sq < x) l = m+1;
+            else r = m-1;
+        }
+        return r;
     }
 }
