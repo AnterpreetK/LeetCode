094b6ff9a0a8e9e3aa73e519d19e41221878e608
Anterpreet Kaur <156046690+AnterpreetK@users.noreply.github.com>
2025-07-03 15:01:44 +0530

    Time: 1 ms (98.4%), Space: 41.8 MB (87.99%) - LeetHub

--- 1873-longest-nice-substring/1873-longest-nice-substring.java
+++ 1873-longest-nice-substring/1873-longest-nice-substring.java
@@ -1,34 +1,15 @@
 class Solution {
     public String longestNiceSubstring(String s) {
-        int maxlen = 0;
-        String res = "";
+        if(s.length() < 2) return "";
 
-        for(int left = 0; left < s.length(); left++){
-            Set<Character> set = new HashSet<>();
-            for(int right = left; right < s.length(); right++){
-                set.add(s.charAt(right));
-
-                if(isNice(set)){
-                    int windowlen = right - left + 1;
-                    if(windowlen > maxlen){
-                        maxlen = windowlen;
-                        res = s.substring(left,right+1);
-                    }
-                }
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(s.indexOf(Character.toLowerCase(c)) == -1 || s.indexOf(Character.toUpperCase(c)) == -1){
+                String left = longestNiceSubstring(s.substring(0,i));
+                String right = longestNiceSubstring(s.substring(i + 1));
+                return left.length() >= right.length() ? left : right;
             }
         }
-        return res;
-    }
-
-    private boolean isNice(Set<Character> set){
-        for(char c : set){
-            if(Character.isLowerCase(c) && !set.contains(Character.toUpperCase(c))){
-                return false;
-            }
-            if(Character.isUpperCase(c) && !set.contains(Character.toLowerCase(c))){
-                return false;
-            }
-        }
-        return true;
+        return s;
     }
 }
