74bf88fc49817cb1fd3a3d2919fd3d65d9c085e5
Anterpreet Kaur <156046690+AnterpreetK@users.noreply.github.com>
2025-08-09 01:32:24 +0530

    Time: 17 ms (34.14%), Space: 42.8 MB (23.04%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -9,18 +9,18 @@ class Solution {
             char cs = s.charAt(i);
             char ct = t.charAt(i);
 
-            if(st.containsKey(cs)) {
+            if(st.containsKey(cs)){
                 if(st.get(cs) != ct) return false;
             } else {
-                st.put(cs, ct);
-            }
+                st.put(cs,ct);
+            } 
 
-            if(tt.containsKey(ct)) {
+            if(tt.containsKey(ct)){
                 if(tt.get(ct) != cs) return false;
             } else {
                 tt.put(ct,cs);
             }
-        } 
+        }
         return true;
     }
 }
