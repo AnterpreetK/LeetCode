dbdeb9375cb4b698f7d6b73ff05cd968cb09562d
Anterpreet Kaur <156046690+AnterpreetK@users.noreply.github.com>
2025-12-23 22:32:22 +0530

    Time: 8 ms (32.38%), Space: 44.2 MB (78.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,15 +2,13 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
 
-        int[] count = new int[26];
-
+        int[] cnt = new int[26];
         for(int i = 0; i < s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+            ++cnt[s.charAt(i) - 'a'];
+            --cnt[t.charAt(i) - 'a'];
         }
-
-        for(int c : count){
-            if(c != 0) return false;
+        for(int i = 0; i < 26; i++){
+            if(cnt[i] != 0) return false;
         }
         return true;
     }
